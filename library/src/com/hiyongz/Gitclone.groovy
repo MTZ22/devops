7df2c3243dbe4a849f05e18a77c0775009e5236e
mtz <matiezhu1998@163.com>
2024-05-11 14:20:50 +0800

    add pack

--- library/src/com/hiyongz/Gitclone.groovy
+++ library/src/com/hiyongz/Gitclone.groovy
@@ -1,3 +1,4 @@
-def gitClone(git_branch,git_url){
-    git branch: "${git_branch}", credentialsId: 'TECHSUN-TFS-JENKINS', url: '${git_url}'
+package org.foo;
+def checkOutFrom(branch,repo) {
+  git branch: "$branch", credentialsId: 'TECHSUN-TFS-JENKINS', url: "https://tfs2015.techsun.com/tfs/DefaultCollection/Devops%20Teams/_git/$repo"
 }
