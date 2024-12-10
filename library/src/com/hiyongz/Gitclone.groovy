2aabc2aadd7e2874b0d77fdf9be2e40caa397570
mtz <matiezhu1998@163.com>
2024-12-10 14:10:50 +0800

    remove file

--- library/src/com/hiyongz/Gitclone.groovy
+++
@@ -1,4 +0,0 @@
-package org.foo;
-def checkOutFrom(branch,repo) {
-  git branch: "$branch", credentialsId: 'TECHSUN-TFS-JENKINS', url: "https://tfs2015.techsun.com/tfs/DefaultCollection/Devops%20Teams/_git/$repo"
-}
--- library/src/com/hiyongz/MyLib.groovy
+++
@@ -1,7 +0,0 @@
-def getTime() {
-    return new Date().format('yyyy-MM-ddHHmmss')
-}
-
-def getJenkinsHome(){
-	return "${JENKINS_HOME}";
-}
